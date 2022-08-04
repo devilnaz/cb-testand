353862a01ad20b154db9555d0bde6d69102c3c12
Ilnaz Shagvaliev <iln@clientbase.ru>
2022-08-04 18:08:26 +0300

    Отрисовка внешнего вида

--- assets/scripts/stand@2.0.js
+++ assets/scripts/stand@2.0.js
@@ -1,140 +1,203 @@
-class ProductService {
+const { useToast } = primevue.usetoast;
 
-    getProductsSmall() {
-		return fetch('./assets/scripts/products-small.json').then(res => res.json()).then(d => d.data);
-	}
-
-	getProducts() {
-		return fetch('./products.json').then(res => res.json()).then(d => d.data);
-    }
-
-    getProductsWithOrdersSmall() {
-		return fetch('./products-orders-small.json').then(res => res.json()).then(d => d.data);
-	}
-}
-
-
-const template = /* html */`
-
-<div :class="styles.table">
-  <p-datatable :value="products" responsive-layout="scroll">
-      <p-column v-for="col of columns" :field="col.field" :header="col.header" :key="col.field"></p-column>
-  </p-datatable>
-</div>
-
-<!--
-<p-treetable :value="nodes">
-    <template #header>
-        All stands
-    </template>
-    <p-column field="name" header="Title" :expander="true"></p-column>
-    <p-column field="size" header="Master"></p-column>
-    <p-column field="type" header="Branch"></p-column>
-    <p-listbox v-model="selectedCity" :options="cities" option-label="name" style="width:15rem"></p-listbox>
-    <p-column header-style="width: 10rem" header-class="text-center" body-class="text-center">
-        <template #header>
-            <p-button type="button" icon="pi pi-cog"></p-button>
-        </template>
-        <template #body>
-            <p-button type="button" icon="pi pi-search" class="p-button-success" style="margin-right: .5em"></p-button>
-            <p-button type="button" icon="pi pi-pencil" class="p-button-warning"></p-button>
-        </template>
-    </p-column>
-    <template #footer>
-        <div style="text-align:left">
-            <p-button icon="pi pi-refresh"></p-button>
-        </div>
-    </template>
-</p-treetable> -->
-
-<!--
-<div class="card">
-    <p-treetable>
-        <p-column v-for="item in standList">{{item}}</p-column>
-    </p-treetable>
-</div> -->
-
-<!--
-<div class="card">
-    <p-datatable :value="products" responsive-layout="scroll">
-        <p-column field="code" header="Code"></p-column>
-        <p-column field="name" header="Name"></p-column>
-        <p-column field="category" header="Category"></p-column>
-        <p-column field="quantity" header="Quantity"></p-column>
-    </p-datatable>
-</div> -->
-`
-
-const Table = {
-  setup() {
-
-      const productService = Vue.ref(new ProductService());
-
-      Vue.onMounted(() => {
-        productService.value.getProductsSmall().then(data => products.value = data);
-      })
-
-
-      const columns = Vue.ref([
-          {field: 'code', header: 'Наименование'},
-          {field: 'name', header: 'Master'},
-          {field: 'category', header: 'Ветка'},
-          {field: 'quantity', header: ''}
-      ]);
-      const products = Vue.ref();
-
-      return { columns, products }
-  },
-  data() {
-    return {
-      request: {
-        getStandsInfo: {
-          url: 'https://ts.cbkeys.ru/api/getStandsInfo.php',
-          response: '',
-        },
-      },
-      standList: [],
-      styles: {
-        table: [
-          'md:w-9',
-          'ml-auto',
-          'mr-auto'
-        ]
+const standButtons = {
+  props: {
+    data: {
+      type: Object,
+      default() {
+        return {
+          name: 'undefined',
+          master: 'ADMIN',
+          branch: 'The ya`best'
+        }
       }
     }
   },
-  watch: {
-    standList() {
-      console.log('Данные изменились');
-      console.log(this.standList);
-    }
-  },
-  computed: {
+  components: {
+    "p-menu": primevue.menu,
+    "p-button": primevue.button,
+    "p-toast": primevue.toast
 
   },
-  methods: {
-    async getStandsInfo() {
-      let test = await fetch(this.request.getStandsInfo.url)
-        .then(response => response.json())
-        .then(commits => this.standList.push(commits));
-      // this.test();
-      // this.standList = test;
-    },
-    test() {
-      console.log(this.request.getStandsInfo.response);
+
+  setup({ data }) {
+    console.log(data);
+    const styles = {
+      button: ['stands__btn', 'stands__btn_change'],
+    };
+  
+    const toast = useToast();
+
+    const menu = Vue.ref();
+
+    const items = Vue.ref([
+       
+            {
+              label: 'Сброс',
+              icon: 'pi pi-refresh',
+              command: () => {
+                  toast.add({severity:'success', summary:'Updated', detail:'Data Updated', life: 3000});
+              }
+            },
+            {
+              label: 'Composer install',
+              icon: 'pi pi-times',
+              command: () => {
+                  toast.add({ severity: 'warn', summary: 'Delete', detail: 'Data Deleted', life: 3000});
+              }
+            },
+            {
+              label: 'Composer update',
+              icon: 'pi pi-times',
+              command: () => {
+                  toast.add({ severity: 'warn', summary: 'Delete', detail: 'Data Deleted', life: 3000});
+              }
+            }
+    ]);
+
+    function changeBranch (event) {
+      console.log(event.target);
+      console.log(event.target.classList.contains('stands__btn_change'));
+    }
+
+    const toggle = (event) => {
+        menu.value.toggle(event);
+    };
+    const save = () => {
+        toast.add({severity: 'success', summary: 'Success', detail: 'Data Saved', life: 3000});
+    };
+
+
+
+    return {
+      styles, items, menu, toggle, save, changeBranch
     }
   },
-  mounted() {
-    this.getStandsInfo();
-  },
-  components: {
-      "p-datatable": primevue.datatable,
-      "p-column": primevue.column
-  },
-  template,
+  template: /*html*/`
+    <span class="p-buttonset">
+        <p-button :class="styles.button" icon="pi pi-share-alt" @click="changeBranch($event)"></p-button>
+        <p-button :class="styles.button" icon="pi pi-arrow-left"></p-button>
+        <p-button :class="styles.button" icon="pi pi-sync"></p-button>
+        <p-button type="button" icon="pi pi-bars" @click="toggle" aria-haspopup="true" aria-controls="overlay_menu"></p-button>
+        <p-menu id="overlay_menu" ref="menu" :model="items" :popup="true">
+        </p-menu>
+    </span>
+    
+  `
 };
 
-const standTable = Vue.createApp(Table)
-standTable.use(primevue.config.default)
+const Table = {
+  components: {
+    "p-datatable": primevue.datatable,
+    "p-column":    primevue.column,
+    'stand-buttons': standButtons,
+  },
+
+
+  setup(props) {
+    const stands = Vue.ref([]);
+    
+    Vue.onMounted(async () => {
+      let response = await fetch('https://ts.cbkeys.ru/api/getStandsInfo.php');
+      let result = await response.json();
+
+      if (result.length > 0) {
+        // console.log(stands);
+        stands.value = result.map(stand => {
+          return {
+            name:   stand.name,
+            master: stand.master,
+            branch: stand.branch,
+          }
+        });
+      }
+    });
+
+  const styles = {
+    table: [
+      'md:w-9',
+      'ml-auto',
+      'mr-auto'
+    ],
+    branch: [
+      'stands__branch-link',
+      'text-white-alpha-90',
+      'no-underline',
+      'hover:text-purple-200',
+    ],
+  };
+
+  // Для теста
+    const stockClass = (data) => {
+        return [
+            {
+                'class-master': data.master == 'master',
+            }
+        ];
+    };
+
+    const standLink = (dataRow) => {
+      return [
+        'https://ts.cbkeys.ru/' + dataRow.name,
+      ];
+    };
+    
+
+    return { 
+      columns: Vue.computed(() => [
+        {field: 'name', header: 'Наименование'},
+        {field: 'master', header: 'Master'},
+        {field: 'branch', header: 'Ветка'},
+        {field: 'buttons', header: 'Кнопки'}
+      ]),
+      styles,
+      stands,
+      stockClass,
+      standLink
+    }
+  },
+
+
+  template: /*html*/`
+  <div :class="styles.table">
+    <p-datatable :value="stands" responsive-layout="scroll">
+        <!--
+        <p-column 
+          v-for="col of columns" 
+          :key="col.field"
+          :field="col.field" 
+          :header="col.header" 
+        ></p-column> -->
+
+        <p-column field="name" header="Наименование"></p-column>
+        <p-column field="master" header="Master">
+          <template #body="standProps">
+              <div :class="stockClass(standProps.data)">
+                  {{standProps.data.master}}
+              </div>
+          </template>
+        </p-column>
+        <p-column field="branch" header="Ветка">
+          <template #body="standProps">
+            <a :href="standLink(standProps.data)" :class="styles.branch">
+              {{ standProps.data.branch }}
+            </a>
+          </template>
+        </p-column>
+        <p-column field="buttons" header="Кнопки">
+          <template #body="standProps">
+            <stand-buttons 
+              :data="standProps.data.name == 'tsdb_03' ? undefined : standProps.data"
+            ></stand-buttons>
+          </template>
+        </p-column>
+    </p-datatable>
+  </div>
+`,
+};
+
+const standTable = Vue.createApp(Table);
+standTable.use(primevue.config.default);
+standTable.use(primevue.toastservice);
 standTable.mount("#stands");
 
