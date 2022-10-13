5a6153bfa623522bdd378aba9884c082fea83143
Ilnaz Shagvaliev <iln@clientbase.ru>
2022-10-13 17:56:35 +0300

    Работа со статусами

--- assets/scripts/stand@2.0.js
+++ assets/scripts/stand@2.0.js
@@ -1,98 +1,5 @@
 const { useToast } = primevue.usetoast;
 
-const standButtons = {
-  props: {
-    data: {
-      type: Object,
-      default() {
-        return {
-          name: 'undefined',
-          master: 'ADMIN',
-          branch: 'The ya`best'
-        }
-      }
-    }
-  },
-  components: {
-    "p-menu": primevue.menu,
-    "p-button": primevue.button,
-    "p-toast": primevue.toast,
-  },
-
-  setup({ data }) {
-    // console.log(data);
-    const checked = Vue.ref(false);
-    
-    const styles = {
-      button: ['stands__btn', 'stands__btn_change'],
-    };
-  
-    const toast = useToast();
-
-    const menu = Vue.ref();
-
-    const items = Vue.ref([
-      {
-        label: 'Сброс',
-        icon: 'pi pi-refresh',
-        command: () => {
-            toast.add({severity:'success', summary:'Updated', detail:'Data Updated', life: 3000});
-        }
-      },
-      {
-        label: 'Composer install',
-        icon: 'pi pi-times',
-        command: () => {
-            toast.add({ severity: 'warn', summary: 'Delete', detail: 'Data Deleted', life: 3000});
-        }
-      },
-      {
-        label: 'Composer update',
-        icon: 'pi pi-times',
-        command: () => {
-            toast.add({ severity: 'warn', summary: 'Delete', detail: 'Data Deleted', life: 3000});
-        }
-      }
-    ]);
-
-    // changeBranch($event)
-    function changeBranch (event) {
-      console.log(event.target);
-      console.log(event.target.classList.contains('stands__btn_change'));
-    }
-    
-    function isChecked () {
-      return checked.value ? 'pi pi-caret-right' : 'pi pi-share-alt';
-    }
-    
-    function changeColor () {
-      return checked.value ? 'background-color: var(--blue-400);' : '';
-    }
-
-    const toggle = (event) => {
-        menu.value.toggle(event);
-    };
-    const save = () => {
-        toast.add({severity: 'success', summary: 'Success', detail: 'Data Saved', life: 3000});
-    };
-
-    return {
-      styles, items, menu, toggle, save, changeBranch, checked, isChecked, changeColor
-    }
-  },
-  
-  template: /*html*/`
-    <span class="p-buttonset">
-      <p-button @click.self="changeBranch" @click="$emit('testCkc')" @click="checked = !checked" :class="styles.button" :style="changeColor()" :icon="isChecked()" ></p-button>
-      <p-button :class="styles.button" icon="pi pi-arrow-left"></p-button>
-      <p-button :class="styles.button" icon="pi pi-sync"></p-button>
-      <p-button type="button" icon="pi pi-bars" @click="toggle" aria-haspopup="true" aria-controls="overlay_menu"></p-button>
-      <p-menu id="overlay_menu" ref="menu" :model="items" :popup="true">
-      </p-menu>
-    </span>
-  `
-};
-
 const TwoCell = {
   components: {
     "p-menu": primevue.menu,
@@ -134,36 +41,23 @@ const TwoCell = {
       }
     ]);
     
-    const countries = Vue.ref([
-      {"name": "Afghanistan", "code": "AF"},
-      {"name": "Åland Islands", "code": "AX"},
-      {"name": "Albania", "code": "AL"},
-      {"name": "Algeria", "code": "DZ"},
-      {"name": "American Samoa", "code": "AS"},
-      {"name": "Andorra", "code": "AD"},
-      {"name": "Angola", "code": "AO"},
-      {"name": "Anguilla", "code": "AI"},
-      {"name": "Antarctica", "code": "AQ"},
-      {"name": "Antigua and Barbuda", "code": "AG"},
-      {"name": "Argentina", "code": "AR"},
-    ]);
-    
     const selectedCountry1 = Vue.ref();
     const filteredCountries = Vue.ref();
     
     const searchCountry = (event) => {
-        setTimeout(() => {
-            if (!event.query.trim().length) {
-              console.log('first');
-                filteredCountries.value = [...allDataBranch.value];
-            }
-            else {
-              console.log('second');
-                filteredCountries.value = countries.value.filter((country) => {
-                    return country.name.toLowerCase().startsWith(event.query.toLowerCase());
-                });
-            }
-        }, 250);
+      
+        if (!event.query.trim().length) {
+          console.log('first');
+          
+          getAllBranch();
+        }
+        else {
+          console.log('second');
+          
+          filteredCountries.value = allDataBranch.value.filter((branch) => {
+            return branch.name.toLowerCase().startsWith(event.query.toLowerCase());
+          });
+        }
     };
         
     const toggle = (event) => {
@@ -173,17 +67,37 @@ const TwoCell = {
         toast.add({severity: 'success', summary: 'Success', detail: 'Data Saved', life: 3000});
     };
     
-    const chooseBranch = Vue.ref(false);
+    const statusBranch = Vue.ref(1);
     
     function changeBranch(event) {
-      // console.log(event.target);
+      console.log(event.target);
       // console.log(event.target.classList.contains('stands__btn_change'));
       
-      chooseBranch.value = !chooseBranch.value;
       
-      console.log('chooseBranch.value: ', chooseBranch.value);
+      if (statusBranch.value === 1) {
+        statusBranch.value = 2;
+      } else if (statusBranch.value === 2) {
+        if (event.target.classList.contains('stands__btn_change')) {
+          event.target.setAttribute('disabled', 'disabled');
+        } else {
+          event.target.parentElement.setAttribute('disabled', 'disabled');
+        }
+        // statusBranch.value = 3;
+      } else if (statusBranch.value === 3) {
+        statusBranch.value = 1;
+      }
       
-      styles.hidden[0]['hidden-element'] = chooseBranch.value;
+      console.log(statusBranch.value);
+      
+      
+      // statusBranch.value = !statusBranch.value;
+      // if (statusBranch.value) {
+        
+      // }
+      
+      // console.log('statusBranch.value: ', statusBranch.value);
+      
+      // styles.hidden[0]['hidden-element'] = statusBranch.value;
       
     }
     
@@ -197,33 +111,57 @@ const TwoCell = {
           body: data
       });
       let result = await response.json();
-      console.log(result);
-      if (result.length > 0) {
-        // console.log(stands);
-        for (let branch of result.branches) {
-          allDataBranch.value = {
-            label: branch,
-            value: branch,
-          }
-        };
-      }
-      console.log('allDataBranch.value: ', allDataBranch.value);
-    }
+      
+      // console.log(result.branches);
+      for (let branch of result.branches) {
+        allDataBranch.value.push({
+          name: branch,
+          value: branch,
+        })
+      };
+      // console.log(allDataBranch.value);
+      
+      filteredCountries.value = [...allDataBranch.value];
+    };
     
-    Vue.onMounted(getAllBranch);
+    async function onBackToMaster(id) {
+        console.log(this.$store.state.stands[id]);
+        let stand = this.$store.state.stands[id];
+        data = new FormData;
+        data.append('branch', stand.branch);
+        data.append('route', stand.route);
+        data.append('clear', 1);
+        let response = await fetch('https://ts.cbkeys.ru/api/changeBranch.php', {
+            method: "POST",
+            body: data
+        });
+        let result = await response.json();
+        if(result.ok) {
+            this.$store.dispatch("fetchStands");
+        } else {
+            alert('Ошибка при очистке, обратитесь к администратору!');
+            this.$store.dispatch("fetchStands");
+        }
+    };
     
     
     function changeIcon () {
-      return chooseBranch.value ? 'pi pi-caret-right' : 'pi pi-share-alt';
+      if (statusBranch.value === 1) {
+        return 'pi pi-share-alt';
+      } else if (statusBranch.value === 2) {
+        return 'pi pi-caret-right';
+      }
     }
     
     function changeColor () {
-      return chooseBranch.value ? 'background-color: var(--blue-400);' : '';
+      if (statusBranch.value === 2) {
+        return 'background-color: var(--blue-400);';
+      }
     }
     
-    function changeHidden () {
-      return chooseBranch.value ? 'display: none;' : '';
-    }
+    // function changeHidden () {
+    //   return statusBranch.value == 1 ? 'display: none;' : '';
+    // }
     
     function inputSelect (input) {
       console.log(input.value);
@@ -236,16 +174,8 @@ const TwoCell = {
         'no-underline',
         'hover:text-purple-200',
       ],
-      hidden: [
-        {'hidden-element': chooseBranch.value}
-      ],
       button: ['stands__btn', 'stands__btn_change'],
     });
-    
-    const hiddenStyles = {
-      'display': 'none',
-      'hidden-element': chooseBranch.value,
-    };
     
     return { 
       selectedCountry1,
@@ -256,30 +186,31 @@ const TwoCell = {
       toggle, 
       save, 
       styles,
-      hiddenStyles,
       changeBranch, 
       changeIcon, 
       changeColor, 
-      changeHidden, 
-      chooseBranch, 
+      // changeHidden, 
+      statusBranch, 
       inputSelect,
+      allDataBranch
     };
   },
   
   template: /*html*/`
     <div class="stand__cell">
       <div>
-        <a :href="'https://ts.cbkeys.ru/' + standsProp.data.name" :class="[styles.branch, styles.hidden]" target="_blank">
+        <a :href="'https://ts.cbkeys.ru/' + standsProp.data.name" :class="[styles.branch, [(statusBranch === 2) ? 'hidden-element' : '']]" target="_blank">
           {{ standsProp.data.branch }}
         </a>
       </div>
-      <p-autocomplete @item-select="inputSelect($event)" :class="[chooseBranch ? '' : 'hidden-element']" class="p-inputtext-sm" forceSelection v-model="selectedCountry1" :suggestions="filteredCountries" @complete="searchCountry($event)" optionLabel="name" completeOnFocus="true"></p-autocomplete>
+      
+      <p-autocomplete @item-select="inputSelect($event)" :class="[(statusBranch === 1) ? 'hidden-element' : '']" class="p-inputtext-sm" forceSelection v-model="selectedCountry1" :suggestions="filteredCountries" @complete="searchCountry($event)" optionLabel="name" completeOnFocus="true"></p-autocomplete>
       
       <div class="stand__buttonset">
         <span class="p-buttonset">
-          <p-button @click="changeBranch" :class="styles.button" :style="changeColor()" :icon="changeIcon()"></p-button>
-          <p-button :class="styles.button" icon="pi pi-arrow-left"></p-button>
-          <p-button :class="styles.button" icon="pi pi-sync"></p-button>
+          <p-button @click="changeBranch" :class="styles.button" :style="changeColor()" :icon="changeIcon()" title="Изменить ветку"></p-button>
+          <p-button :class="styles.button" icon="pi pi-arrow-left" title="Очистить (до master)"></p-button>
+          <p-button :class="styles.button" icon="pi pi-sync" title="Обновить (Pull)"></p-button>
           <p-button type="button" icon="pi pi-bars" @click="toggle" aria-haspopup="true" aria-controls="overlay_menu"></p-button>
           <p-menu id="overlay_menu" ref="menu" :model="items" :popup="true">
           </p-menu>
@@ -293,7 +224,7 @@ const Table = {
   components: {
     'p-datatable': primevue.datatable,
     'p-column':    primevue.column,
-    'stand-buttons': standButtons,
+    // 'stand-buttons': standButtons,
     'two-cell': TwoCell,
     "p-autocomplete": primevue.autocomplete
   },
@@ -305,7 +236,7 @@ const Table = {
     Vue.onMounted(async () => {
       let response = await fetch('https://ts.cbkeys.ru/api/getStandsInfo.php');
       let result = await response.json();
-      console.log('result: ', result);
+      // console.log('result: ', result);
 
       if (result.length > 0) {
         // console.log(stands);
